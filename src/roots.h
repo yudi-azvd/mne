cf4f53b9d037278fa413ea0fcce8691e1903e3a6
Yudi Yamane <yudiyamane@gmail.com>
2023-08-31 03:50:58 -0300

    refactor: move bug para teste separado

--- src/roots.h
+++ src/roots.h
@@ -7,12 +7,16 @@
 // https://iq.opengenus.org/typedef-function-pointer-c/#:~:text=The%20keyword%20typedef%20is%20used,us%20by%20C%2FC%2B%2B.
 typedef float (*Function)(float);
 
-float root_bisection(Function f, float x1 = 0.0, float x2 = 1.0, float abs_err = 0.01) {
+#define ERR_ABS(a, b) (abs(a - b)) // a > b
+#define ERR_REL(a, b) ((a - b) / (a + b)) // a > b
+
+float root_bisection(Function f, float x1 = 0.0, float x2 = 1.0, float rel_err = 0.01) {
     uint iterations = 0;
     float root = 0;
 
-    while (abs(x2 - x1) > abs_err && iterations < MAX_ITER) {
-        root = (x1 + x2)/2;
+    // while (ERR_ABS(x2, x1) > abs_err && iterations < MAX_ITER) {
+    while (ERR_REL(x2, x1) > rel_err && iterations < MAX_ITER) {
+        root = (x1 + x2) / 2;
         if (f(x1) * f(root) < 0) {
             x2 = root;
         } else {
@@ -23,5 +27,4 @@ float root_bisection(Function f, float x1 = 0.0, float x2 = 1.0, float abs_err =
     }
 
     return root;
-    // return 0;
 }
--- tests/roots.test.cpp
+++ tests/roots.test.cpp
@@ -3,12 +3,24 @@
 #include "roots.h"
 #include "test_defs.h"
 
-float f(float x) {
-    return x * x - 2;
+float sqrt_of_2(float x) {
+    return x * x - 2.0;
+}
+
+float sqrt_of_4(float x) {
+    return x * x - 4.0;
 }
 
 TEST_CASE("bisection") {
-    CHECK(approx(1.41406) == root_bisection(f, 0, 2));
-    
-    CHECK(approx_eps(1.41, 0.1) == root_bisection(f, 0, 2));
+    CHECK(approx_eps(1.41406, 0.01) == root_bisection(sqrt_of_2, 0, 2));
+    CHECK(approx_eps(1.41, 0.01) == root_bisection(sqrt_of_2, 0, 2));
+
+    CHECK(approx_eps(2, 0.1) == root_bisection(sqrt_of_4, 0.0, 4.1));
+}
+
+TEST_CASE("bisection exact root") {
+    // FIXME: se o primeiro chute cai certinho na raíz, 2 por exemplo, 
+    // f(2) == 0, não é positivo nem negativo. O teste fica "indefinido".
+    // Deveria ter o teste sign(f(root)) == 0: return root;
+    CHECK(approx_eps(2, 0.1) == root_bisection(sqrt_of_4, 0.0, 4.0));
 }
