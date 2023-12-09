1886e8eed2ecb67ed9892d1c1fdab5bda4df8dc3
Yudi Yamane <yudiyamane@gmail.com>
2023-12-08 21:25:13 -0300

    chore: nem sei

--- mne/exercises/lista_d/logistic_map.py
+++ mne/exercises/lista_d/logistic_map.py
@@ -1,49 +1,41 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-time_samples = 1000
 x0 = 0.4
 x = x0
-r = 2.6
-total_time = 500
-time_span = np.linspace(0, total_time, total_time)
-rates = np.linspace(0, 4, time_samples)
+time_samples = 600
+time_end = time_samples
+time_span = np.linspace(0, time_end, time_samples)
 
-
-list_of_repeated_values = []
+rate_samples = 1000
+r_start = 3.62
+r_end = 3.64
+rate_span = np.linspace(r_start, r_end, rate_samples)
 
 
 def close_to_any(a: float, values: list[float], **kwargs):
     return np.any(np.isclose(a, values, **kwargs))
 
 
-def get_repeated_points(r: float, values: list[float]):
+def get_last_100_values(r: float, values: list[float]):
     xs: list[float] = []
     ys: list[float] = []
 
-    if r < 3:
-        ys.append(values[-1])
+    for v in values[len(values) - 100 : len(values)]:
+        ys.append(v)
         xs.append(r)
-        list_of_repeated_values.append(ys)
-        return xs, ys
-
-    for i, v in reversed(list(enumerate(values))):
-        if close_to_any(v, ys, rtol=0.0000000000000000000000000000001):
-            break
-        else:
-            ys.append(v)
-            xs.append(r)
-
-    list_of_repeated_values.append(ys)
     return xs, ys
 
 
 population_values_r: list[list[float]] = []
 final_populations: list[float] = []
 
+fig, (ax1, ax2) = plt.subplots(1, 2)
+fig.suptitle('Mapa logístico: G(x) = r x (1-x)')
+
 _x = []
 _y = []
-for r in rates:
+for r in rate_span:
     pop_values = []
     x = x0
     for i in range(len(time_span)):
@@ -51,16 +43,15 @@ for r in rates:
         pop_values.append(x)
     population_values_r.append(pop_values)
     final_populations.append(pop_values[-1])
-    xs, ys = get_repeated_points(r, pop_values)
+    xs, ys = get_last_100_values(r, pop_values)
     _x.extend(xs)
     _y.extend(ys)
 
+    # ax1.plot(time_span, pop_values)
+    # ax2.scatter(xs, ys, s=0.05, alpha=0.3, c='k')
+    # plt.show()
 
-i = int(len(rates) * 0.72)
-print(rates[i])
-fig, (ax1, ax2) = plt.subplots(1, 2)
-fig.suptitle('Mapa logístico: G(x) = r x (1-x)')
-
+i = int(len(rate_span) * 0.65)
 ax1.plot(time_span, population_values_r[i])
 ax1.set_ylim((-0.1, 1))
 
